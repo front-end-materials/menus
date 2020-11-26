5ff2a9de1c28616b22aa0ea332e29ca124de8491
Dave Everitt <deveritt@innotts.co.uk>
2020-11-26 20:50:38 +0000

    update tabs and clean up code

--- js-tabs/scripts.js
+++ js-tabs/scripts.js
@@ -4,16 +4,17 @@ const myNav = document.querySelector("nav");
 myButton.addEventListener("click", toggleMenu);
 
 const sections = [sectionOne, sectionTwo, sectionThree, sectionFour];
-const tabs = [triggerOne, triggerTwo, triggerThree, triggerFour];
+const tabs = [trigger01, trigger02, trigger03, trigger04];
 
 function toggleMenu() {
   // console.log("toggled menu button");
   myNav.classList.toggle("hide");
 }
 
-triggerTwo.addEventListener("click", ()=> showSection(sectionTwo, triggerTwo));
-triggerThree.addEventListener("click", ()=> showSection(sectionThree, triggerThree));
-triggerFour.addEventListener("click", ()=> showSection(sectionFour, triggerFour));
+trigger01.addEventListener("click", ()=> showSection(sectionOne, trigger01));
+trigger02.addEventListener("click", ()=> showSection(sectionTwo, trigger02));
+trigger03.addEventListener("click", ()=> showSection(sectionThree, trigger03));
+trigger04.addEventListener("click", ()=> showSection(sectionFour, trigger04));
 
 function showSection(section, tab) {
   // console.log(section, tab);
@@ -22,6 +23,7 @@ function showSection(section, tab) {
   tab.classList.remove("inactive");
   toggleMenu();
   event.preventDefault();
+  // Event.preventDefault();
 }
 
 function hideAllSections() {
